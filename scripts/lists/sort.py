4496e59e77726c36765a1fa4fa0932b2a8b09c87
jeroen <J.A.Boogaard@hr.nl>
2022-07-05 16:17:08 +0200

    Moved scripts to root directory

---
+++ scripts/lists/sort.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+ # Author: Jacques de Hooge
+ 
+# Store 4 strings into a list
+cities = ['Londen', 'Paris', 'New York', 'Berlin'] 
+
+# Verify that it is indeed a list
+print ('Class is:', type (cities))
+
+# Print the unsorted list
+print ('Before sorting:', cities)
+
+# Sort the list
+cities.sort ()
+
+# Print the sorted list
+print ('After sorting: ', cities)
