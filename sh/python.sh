07fbd36e92d2372958f6e00c77dee8413373527a
Jeroen Boogaard <J.A.Boogaard@hr.nl>
2022-07-06 16:11:48 +0200

    Make script to run the container entrypoint

--- sh/python.sh
+++ sh/python.sh
@@ -5,12 +5,14 @@ nr_of_arguments=${#argument_values[@]}
 
 containername="python-ai"
 entrypoint="run_script"
-cmd="docker/run/miw_container.sh ${containername} ${entrypoint}"
 
 # Run with script?
 if [ $nr_of_arguments -gt 0 ]; then
-  cmd="${cmd} \"${argument_values[0]}\""
+  script="${argument_values[0]}"
+  printf "Run with script %s\n" ${script}
+  entrypoint=${script}
   containername="${containername}-script"
 fi
 
+cmd="docker/run/miw_container.sh ${containername} ${entrypoint}"
 eval ${cmd}
