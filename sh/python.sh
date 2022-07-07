0d9ee8c6a36209414002637f7d764ecbbd86bdaf
AlxcNL <J.A.Boogaard@hr.nl>
2022-07-07 21:53:03 +0200

    Use docker-compose to run script script in container

--- sh/python.sh
+++ sh/python.sh
@@ -4,17 +4,18 @@ argument_values=("$@")
 nr_of_arguments=${#argument_values[@]}
 
 containername="python-ai"
+name="jaboo/miw"
+image="${name}:latest"
 entrypoint="run_script"
 
 # Run with script?
 if [ $nr_of_arguments -gt 0 ]; then
   script="${argument_values[0]}"
   printf "Run with script %s\n" ${script}
-  entrypoint=${script}
-  containername="${containername}-script"
+  cmd="(cd docker/run; export SCRIPT=${script}; export IMAGE=${image}; docker-compose down; docker-compose up python-script)"
+else
+  cmd="(docker run -it --rm --name python-repl --entrypoint=${entrypoint} ${image})"
 fi
-
-cmd="(cd docker/run; export SCRIPT=${script}; docker-compose up python-script)"
 
 echo ${cmd}
 eval ${cmd}
