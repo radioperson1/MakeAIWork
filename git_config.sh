26d38563c8509197ed210ceecc1e30b7746cc0cc
AlxcNL <J.A.Boogaard@hr.nl>
2022-07-13 09:13:43 +0200

    Added script to configure git

---
+++ git_config.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+argument_names=("name" "email")
+argument_values=("$@")
+nr_of_arguments=${#argument_values[@]}
+
+if [ $nr_of_arguments -lt ${#argument_names[@]} ]; then
+    printf "USAGE: %s %s\n" "$0" "${argument_names[*]}"
+    exit 1;
+fi
+
+git --version
+git config user.name "${argument_values[0]}"
+git config user.email "${argument_values[1]}"
+git config --list
