17085858b33aebdfc91981c1431454b86d9c30d3
AlxcNL <J.A.Boogaard@hr.nl>
2022-07-22 10:38:10 +0200

    Jupyter can now also run in a WSL Linux environment

--- sh/jupyter.sh
+++ sh/jupyter.sh
@@ -2,6 +2,13 @@
 
 cmd="docker/run/miw_container.sh jupyter"
 
+if (! command -v "${cmd}" &> /dev/null) then
+    echo Try locally installed jupyter
+    cmd="(cd notebooks; jupyter-lab)"
+elif (! command -v "${cmd}" &> /dev/null) then 
+    echo "${cmd} could not be found" && exit
+fi
+
 echo "Open the URL in your browser"
 echo "$cmd"
 eval ${cmd} | grep -Eo 'http://127.0.0.1:8888/lab\?.*'
