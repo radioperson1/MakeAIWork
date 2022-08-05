de7fbe4bb99d8e58ea7835c360d9154b9f9dd200
AlxcNL <J.A.Boogaard@hr.nl>
2022-08-05 13:51:38 +0200

    Added directory pics

--- docker/run/miw_container.sh
+++ docker/run/miw_container.sh
@@ -13,12 +13,14 @@ fi
 
 hostdirHome="${PWD}"
 hostdirNotebooks="${hostdirHome}/notebooks"
+hostdirPics="${hostdirHome}/pics"
 hostdirProjects="${hostdirHome}/projects"
 hostdirScripts="${hostdirHome}/scripts"
 
 function makeWindowsProof {
     hostdirNotebooks=$(cygpath -w -p ${hostdirNotebooks})
     hostdirProjects=$(cygpath -w -p ${hostdirProjects})
+    hostdirPics=$(cygpath -w -p ${hostdirPics})
     hostdirScripts=$(cygpath -w -p ${hostdirScripts})
     prefix="winpty "
 }
@@ -36,6 +38,7 @@ case "${os}" in
 esac
 
 export HOSTPATH_NOTEBOOKS=${hostdirNotebooks}
+export HOSTPATH_PICS=${hostdirPics}
 export HOSTPATH_PROJECT=${hostdirProjects}
 export HOSTPATH_SCRIPTS=${hostdirScripts}
 
@@ -44,37 +47,37 @@ export HOSTPATH_SCRIPTS=${hostdirScripts}
 mode="${argumentValues[0]}"
 
 name="jaboo/miw"
-version="0.7"
+version="0.8"
 image="${name}:${version}"
 containerName="python-ai-${mode}"
 containerHome="/home/student"
 containerdirNotebooks="${containerHome}/notebooks"
 containerdirProjects="${containerHome}/projects"
+containerdirPics="${containerHome}/pics"
 containerdirScripts="${containerHome}/scripts"
 composePath="docker/compose"
 graphicsParams="-v \"/tmp/.X11-unix:/tmp/.X11-unix\" -e \"DISPLAY=$DISPLAY\" --net=host"
 
+cmd="${prefix}docker run -it --rm --name ${containerName}"
+cmd="${cmd} -v \"${hostdirNotebooks}:${containerdirNotebooks}\""
+cmd="${cmd} -v \"${hostdirPics}:${containerdirPics}\""
+cmd="${cmd} -v \"${hostdirProjects}:${containerdirProjects}\""
+cmd="${cmd} -v \"${hostdirScripts}:${containerdirScripts}\""
+cmd="${cmd} ${graphicsParams} --entrypoint ${entryPoint} ${image}"
+
 case "${mode}" in
     bash*)
         entryPoint="bash"
-        cmd="${prefix}docker run -it --rm --name ${containerName}"
-        cmd="${cmd} -v \"${hostdirProjects}:${containerdirProjects}\""
-        cmd="${cmd} -v \"${hostdirNotebooks}:${containerdirNotebooks}\""
-        cmd="${cmd} -v \"${hostdirScripts}:${containerdirScripts}\""
-        cmd="${cmd} ${graphicsParams} --entrypoint ${entryPoint} ${image}";;
+        cmd="${cmd} --entrypoint ${entryPoint} ${image}";;
     jupyter*)     
         composefile="${composePath}/python-ai-jupyter.yaml"
         cmd="docker/compose/up.sh ${composefile}";;
     python-repl*)
         entryPoint="ptpython"
-        cmd="${prefix}docker run -it --rm --name ${containerName} --entrypoint ${entryPoint} ${image}";;        
+        cmd="${cmd} --entrypoint ${entryPoint} ${image}";;        
     python-script*)
         entryPoint="run_script"
-        cmd="${prefix}docker run -it --rm --name ${containerName}"
-        cmd="${cmd} -v \"${hostdirProjects}:${containerdirProjects}\""
-        cmd="${cmd} -v \"${hostdirNotebooks}:${containerdirNotebooks}\""
-        cmd="${cmd} -v \"${hostdirScripts}:${containerdirScripts}\""
-        cmd="${cmd} ${graphicsParams} -e SCRIPT=\"${argumentValues[1]}\" --entrypoint ${entryPoint} ${image}";;
+        cmd="${cmd} -e SCRIPT=\"${argumentValues[1]}\" --entrypoint ${entryPoint} ${image}";;
     # Default
     *)      
         cmd="${prefix}docker run --rm --name ${containerName} ${image}";;        
