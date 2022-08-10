5a86f9e7dad67d88f4f259d6f32cb8394e4ddd04
AlxcNL <J.A.Boogaard@hr.nl>
2022-08-10 18:03:17 +0200

    Added instructions to notebook

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-def rekenen(param):
-    print(param, " plus 7 is ", param+7)
-
-def uitrekenen(param1, param2):
+def sum(param1, param2):
+    """
+    Deze functie berekent de som van twee getallen:
+    param1 + param2
+    """
     print(param1, " plus ", param2, " = ", param1+param2)
+
     return param1+param2
 
-rekenen(8)
-resultaat= uitrekenen(15,12)
-print(resultaat)    
+print(sum.__doc__)
+sum(15,12)
