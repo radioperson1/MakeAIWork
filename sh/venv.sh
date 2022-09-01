0f804c6acda497fdc0d7490641ee740a5e6daa85
AlxcNL <J.A.Boogaard@hr.nl>
2022-09-01 15:45:09 +0200

    Merge develop branch

---
+++ sh/venv.sh
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env bash
+
+# Install virtualenv
+python3 -m pip install --upgrade pip
+python3 -m pip install virtualenv
+
+# Create virtual environment env
+python3 -m venv env
+
+# Activate virtual environment env
+source env/bin/activate
+
+# Automatically activate env
+touch ~/.bash_profile && echo "source env/bin/activate" >> ~/.bash_profile
