9d10c1007aedbe8ca16ec5b914deaa3f70576542
AlxcNL <J.A.Boogaard@hr.nl>
2022-09-01 15:43:08 +0200

    Install dependencies with both pip and miniconda

---
+++ sh/install_requirements.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Activate virtual environment env
+source env/bin/activate
+
+# CONDA
+
+# Install tensorflow and lidar
+conda install -c conda-forge tensorflow \
+  tensorflow \
+  tensorboard-plugin-wit \
+  tensorflow-estimator \
+  lidar
+
+#  PIP
+
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Install setuptools
+pip install setuptools
+Install all required libraries t
+pip install -r requirements.txt
