02ffc288d2181c18c1edf5d4dfa62664fd014617
AlxcNL <J.A.Boogaard@hr.nl>
2022-09-05 15:13:45 +0200

    Merge branch 'main' into support

---
+++ sh/install_jupyter.sh
@@ -0,0 +1,13 @@
+python3 -m pip install --upgrade pip
+
+python3 -m pip install virtualenv
+
+python3 -m venv env
+
+# Mac
+source env/bin/activate
+
+# Windows
+source env/Scripts/activate
+
+python3 -m pip install jupyterlab
