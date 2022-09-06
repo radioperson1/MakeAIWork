80e419b7569cb1113aff3fd966d2d87cf6bc177b
AlxcNL <J.A.Boogaard@hr.nl>
2022-09-06 10:40:13 +0200

    Added vector

---
+++ scripts/vector.py
@@ -0,0 +1,13 @@
+class Vector:
+    """
+    This is a class to generate vectors
+    """
+    def __init__(self, scalarList):
+        self.scalarList = scalarList
+    
+    def addVector(self, otherVector):
+        pass
+    
+    def multiplyVector(self, otherVector):
+        pass
+    
