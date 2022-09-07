de1fe2bc877621f4b16204dc96280c2abf4c295a
AlxcNL <J.A.Boogaard@hr.nl>
2022-09-07 16:37:25 +0200

    Added simpylc installation

---
+++ scripts/car.py
@@ -0,0 +1,10 @@
+class Car:    
+    def __init__(self, brand, acceleration, maxSpeed):
+        self.brand = brand
+        self.speed = 0
+        self.x = 0
+        
+    # period = delta t        
+    def move(self, period):
+        # Bereken nieuwe snelheid
+        pass
---
+++ scripts/world.py
@@ -0,0 +1,8 @@
+import Car as car
+
+class World:    
+    def __init__(self):
+        tesla = car("Tesla", 10, (150*3.6))        
+        bmw = car("BMW", 5, (220*3.6))
+        
+        
