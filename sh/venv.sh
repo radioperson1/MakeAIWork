cfe05e9b12e3b12c9e4a27cc019fc03b17e34a0b
AlxcNL <J.A.Boogaard@hr.nl>
2022-09-11 14:31:22 +0200

    Added requirements installation using conda

--- sh/venv.sh
+++ sh/venv.sh
@@ -7,5 +7,30 @@ python3 -m pip install virtualenv
 # Create virtual environment env
 python3 -m venv env
 
+activatePath=""
+
+# Detect OS
+unameOut="$(uname -s)"
+os="${unameOut:0:7}"
+case "${os}" in
+    Linux*)     
+        machine="Linux"
+        activatePath="${PWD}/env/bin/activate"
+        ;;
+    Darwin*)    
+        machine="Mac"
+        activatePath="${PWD}/env/bin/activate"
+    ;;
+    MINGW*)     
+        machine="Git Bash"
+        activatePath="${PWD}/env/Scripts/activate"
+    ;;
+    *)          
+        activatePath="${PWD}/env/bin/activate"
+esac
+
 # Activate virtual environment env
-source env/bin/activate
+source ${activatePath}
+
+# Automatically activate env
+touch ~/.bash_profile && echo "source ${activatePath}" >> ~/.bash_profile
