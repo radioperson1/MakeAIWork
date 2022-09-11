2e22984294c274c12c87dd30bea7971b4de2da1a
AlxcNL <J.A.Boogaard@hr.nl>
2022-09-11 22:03:45 +0200

    Refactroing

--- sh/jupyter.sh
+++ sh/jupyter.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cmd="(cd notebooks; python -m jupyterlab)"
+cmd="(python -m jupyterlab)"
 
 printf "%s cmd : \n\t%s\n\n" "$0" "${cmd}"
 eval ${cmd}
--- sh/simpylc.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sh/python.sh simulations/car/world.py
